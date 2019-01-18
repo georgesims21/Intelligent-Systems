c7478bfb2bd4bc060bfac3a399baeb85a4b5df5c
George Sims <georgesims21@hotmail.com>
2019-01-18 14:37:27 +0100

    Strange error only cheap playing

--- bots/kbbot/kbbot.py
+++ bots/kbbot/kbbot.py
@@ -65,6 +65,7 @@ class Bot:
                     # Uncomment the next line if you want to see that something happens.
                     print ("ACE Strategy Applied")
                     return move
+
             # No aces then jacks
             for move in moves:
 
@@ -73,6 +74,15 @@ class Bot:
                     # into account that there might be other valid moves according to the strategy.
                     # Uncomment the next line if you want to see that something happens.
                     print ("JACK Strategy Applied")
+                    return move
+
+            for move in moves:
+
+                if not self.cheap_consistent(state, move):
+                    # Plays the first move that makes the kb inconsistent. We do not take
+                    # into account that there might be other valid moves according to the strategy.
+                    # Uncomment the next line if you want to see that something happens.
+                    print ("CHEAP Strategy Applied")
                     return move
 
             # If no move that is entailed by the kb is found, play random move
@@ -146,3 +156,36 @@ class Bot:
         # entailed (proof by refutation)
 
         return akb.satisfiable()
+
+    def cheap_consistent(self, state, move):
+        # type: (State, move) -> bool
+
+        # each time we check for consistency we initialise a new knowledge-base
+        kb = KB()
+
+        # Add general information about the game
+        load.cheap_information(kb)
+
+        # Add the necessary knowledge about the strategy
+        load.cheap_knowledge(kb)
+
+        # This line stores the index of the card in the deck.
+        # If this doesn't make sense, refer to _deck.py for the card index mapping
+        index = move[0]
+
+        # This creates the string which is used to make the strategy_variable.
+        # Note that as far as kb.py is concerned, two objects created with the same
+        # string in the constructor are equivalent, and are seen as the same symbol.
+        # Here we use "pj" to indicate that the card with index "index" should be played with the
+        # PlayJack heuristics that was defined in class. Initialise a different variable if 
+        # you want to apply a different strategy (that you will have to define in load.py)
+        variable_string = "p" + str(index)
+        strategy_variable = Boolean(variable_string)
+
+        # Add the relevant clause to the loaded knowledge base
+        kb.add_clause(~strategy_variable)
+
+        # If the knowledge base is not satisfiable, the strategy variable is
+        # entailed (proof by refutation)
+
+        return kb.satisfiable()
