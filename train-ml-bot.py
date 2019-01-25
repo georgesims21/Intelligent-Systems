c3121bfff9e89f4f0b0d94ca997aaaa74dbd4d0b
George Sims <georgesims21@hotmail.com>
2019-01-25 12:21:50 +0100

    After most recent update

--- train-ml-bot.py
+++ train-ml-bot.py
@@ -6,94 +6,154 @@ Every observed state is converted to a feature vector and labeled with the event
 This is part of the second worksheet.
 """
 from api import State, util
+import pickle
+import os.path
+from argparse import ArgumentParser
+import time
+import sys
 
 # This package contains various machine learning algorithms
-import sys
 import sklearn
 import sklearn.linear_model
-
-import os
-
+from sklearn.neural_network import MLPClassifier
 from sklearn.externals import joblib
 
-import random, os
 from bots.rand import rand
-from bots.bully import bully
-from bots.rdeep import rdeep
-from bots.ml import ml
+# from bots.rdeep import rdeep
+
 from bots.ml.ml import features
-# How many games to play
-GAMES = 1000
 
-# Which phase the game starts in
-PHASE = 1
+def create_dataset(path, player=rand.Bot(), games=2000, phase=1):
 
-# The player we'll observe
-player = ml.Bot(model_file='./bots/ml/rdeep.pkl')
-# player2 = ml.Bot(model_file='./models/rdeep-model.pkl')
+    data = []
+    target = []
 
-data = []
-target = []
+    # For progress bar
+    bar_length = 30
+    start = time.time()
 
-for g in range(GAMES):
+    for g in range(games-1):
 
-    # Randomly generate a state object starting in specified phase.
-    state = State.generate(phase=PHASE)
+        # For progress bar
+        if g % 10 == 0:
+            percent = 100.0*g/games
+            sys.stdout.write('\r')
+            sys.stdout.write("Generating dataset: [{:{}}] {:>3}%".format('='*int(percent/(100.0/bar_length)),bar_length, int(percent)))
+            sys.stdout.flush()
 
-    state_vectors = []
+        # Randomly generate a state object starting in specified phase.
+        state = State.generate(phase=phase)
 
-    while not state.finished():
+        state_vectors = []
 
-        # Give the state a signature if in phase 1, obscuring information that a player shouldn't see.
-        given_state = state.clone(signature=state.whose_turn()) if state.get_phase() == 1 else state
+        while not state.finished():
 
-        # Add the features representation of a state to the state_vectors array
-        state_vectors.append(features(given_state))
+            # Give the state a signature if in phase 1, obscuring information that a player shouldn't see.
+            given_state = state.clone(signature=state.whose_turn()) if state.get_phase() == 1 else state
 
-        # Advance to the next state
-        move = player.get_move(given_state)
-        state = state.next(move)
+            # Add the features representation of a state to the state_vectors array
+            state_vectors.append(features(given_state))
 
-    winner, score = state.winner()
+            # Advance to the next state
+            move = player.get_move(given_state)
+            state = state.next(move)
 
-    for state_vector in state_vectors:
-        data.append(state_vector)
+        winner, score = state.winner()
 
-        if winner == 1:
-            result = 'won'
+        for state_vector in state_vectors:
+            data.append(state_vector)
 
-        elif winner == 2:
-            result = 'lost'
+            if winner == 1:
+                result = 'won'
 
-        target.append(result)
+            elif winner == 2:
+                result = 'lost'
 
-    sys.stdout.write(".")
-    sys.stdout.flush()
-    if g % (GAMES/10) == 0:
-        print("")
-        print('game {} finished ({}%)'.format(g, (g/float(GAMES)*100)))
+            target.append(result)
 
-# Train a logistic regression model
-learner = sklearn.linear_model.LogisticRegression()
-#learner = sklearn.naive_bayes.GaussianNB()
+    with open(path, 'wb') as output:
+        pickle.dump((data, target), output, pickle.HIGHEST_PROTOCOL)
 
-model = learner.fit(data, target)
+    # For printing newline after progress bar
+    print("\nDone. Time to generate dataset: {:.2f} seconds".format(time.time() - start))
 
-# model = model + model2
+    return data, target
 
 
-# Check for class imbalance
-count = {}
-for str in target:
-    if str not in count:
-        count[str] = 0
-    count[str] += 1
+## Parse the command line options
+parser = ArgumentParser()
 
-print('instances per class: {}'.format(count))
+parser.add_argument("-d", "--dset-path",
+                    dest="dset_path",
+                    help="Optional dataset path",
+                    default="dataset.pkl")
+
+parser.add_argument("-m", "--model-path",
+                    dest="model_path",
+                    help="Optional model path. Note that this path starts in bots/ml/ instead of the base folder, like dset_path above.",
+                    default="model.pkl")
+
+parser.add_argument("-o", "--overwrite",
+                    dest="overwrite",
+                    action="store_true",
+                    help="Whether to create a new dataset regardless of whether one already exists at the specified path.")
+
+parser.add_argument("--no-train",
+                    dest="train",
+                    action="store_false",
+                    help="Don't train a model after generating dataset.")
 
 
-with open('./bots/ml/ml2-model.pkl', 'wb') as fo:
-    joblib.dump(model, fo)
-# Store the model in the ml directory DO WE NEED TO CLOSE THIS?
+options = parser.parse_args()
 
-print('Done')
+if options.overwrite or not os.path.isfile(options.dset_path):
+    create_dataset(options.dset_path, player=rand.Bot(), games=10000)
+
+if options.train:
+
+    # Play around with the model parameters below
+
+    # HINT: Use tournament fast mode (-f flag) to quickly test your different models.
+
+    # The following tuple specifies the number of hidden layers in the neural
+    # network, as well as the number of layers, implicitly through its length.
+    # You can set any number of hidden layers, even just one. Experiment and see what works.
+    hidden_layer_sizes = (64, 32)
+
+    # The learning rate determines how fast we move towards the optimal solution.
+    # A low learning rate will converge slowly, but a large one might overshoot.
+    learning_rate = 0.0001
+
+    # The regularization term aims to prevent overfitting, and we can tweak its strength here.
+    regularization_strength = 0.0001
+
+    #############################################
+
+    start = time.time()
+
+    print("Starting training phase...")
+
+    with open(options.dset_path, 'rb') as output:
+        data, target = pickle.load(output)
+
+    # Train a neural network
+    learner = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, learning_rate_init=learning_rate, alpha=regularization_strength, verbose=True, early_stopping=True, n_iter_no_change=6)
+    # learner = sklearn.linear_model.LogisticRegression()
+
+    model = learner.fit(data, target)
+
+    # Check for class imbalance
+    count = {}
+    for t in target:
+        if t not in count:
+            count[t] = 0
+        count[t] += 1
+
+    print('instances per class: {}'.format(count))
+
+    # Store the model in the ml directory
+    joblib.dump(model, "./bots/ml/" + options.model_path)
+
+    end = time.time()
+
+    print('Done. Time to train:', (end-start)/60, 'minutes.')
